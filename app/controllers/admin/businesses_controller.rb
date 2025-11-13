c4e7467a062fd790c98cec3ec09c4c9950b9edd7
MaiveCL <maive_@hotmail.com>
2025-11-13 13:13:35 -0500

    correction lab3

--- app/controllers/admin/businesses_controller.rb
+++ app/controllers/admin/businesses_controller.rb
@@ -1,44 +1,14 @@
 # Marie-Ève — 2025-11-13
 class Admin::BusinessesController < Admin::BaseController
     def index
-        @businesses = Business.order(name: :asc)
+        @businesses = Business.order(name: :desc)
 
         respond_to do |format|
             format.html
-            format.json { render json: @businesses.to_json }
-            format.xml  { render xml: @businesses.as_json.to_xml }
         end
     end
 
     def show
         @business = Business.find(params[:id])
-
-        respond_to do |format|
-            format.html
-            format.json {
-            render json: @business.to_json(
-                include: {
-                menus: {
-                    include: {
-                    items: {},
-                    menus: { include: :items }  # sous-menus avec leurs items
-                    }
-                }
-                }
-            )
-            }
-            format.xml {
-            render xml: @business.as_json(
-                include: {
-                menus: {
-                    include: {
-                    items: {},
-                    menus: { include: :items }
-                    }
-                }
-                }
-            ).to_xml
-            }
-        end
     end
 end
